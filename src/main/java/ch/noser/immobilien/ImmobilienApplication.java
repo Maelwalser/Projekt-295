6b2fa46de0334dae97470b144ada14ec8a65dc89
Maelwalser <mwa142967@stud.gibb.ch>
2023-02-24 09:11:44 +0100

    Initial Commit

---
+++ src/main/java/ch/noser/immobilien/ImmobilienApplication.java
@@ -0,0 +1,13 @@
+package ch.noser.immobilien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmobilienApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImmobilienApplication.class, args);
+    }
+
+}
